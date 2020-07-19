ef0ccb2f1d25ceef867e5d8e9b97978bd2dcf634
npetrelli <npetrell@mi-r3.msk.21-school.ru>
2020-07-19 21:24:53 +0300

    add scene

---
+++ Prototipe 8.56.46 PM/Assets/Scripts/PlayScene/Arm.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Arm : MonoBehaviour
+{
+    static private bool tumbler = false;
+    Animator anim;
+    float	dist;
+	public Transform target;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public bool Tumbler
+    {
+        get => tumbler;
+        set {
+            tumbler = value;
+        }
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+		if(target == null) return;
+        Vector3 v = target.position - transform.position;   
+        float sqrX = v.x * v.x;     
+        float sqrZ = v.z * v.z;     
+        float distance = Mathf.Sqrt(sqrX + sqrZ);
+        if (distance <= 2)
+        {
+            anim.SetBool("open", true);
+            Tumbler = true;
+            GameObject.FindGameObjectWithTag("location").GetComponent<Image>().enabled = true;
+            GameObject.FindGameObjectWithTag("location").GetComponent<OpenToPyramid>().sceneEnd = true;
+        }
+        else
+            anim.SetBool("open", false);
+    }
+}
---
+++ Prototipe 8.56.46 PM/Assets/Scripts/Winner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Winner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AudioSource audio;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.Stop();
+    }
+
+    // Update is called once per frame
+    void Accelerate()
+    {
+        SceneManager.LoadScene("Winner", LoadSceneMode.Single);
+    }
+    void OnMouseDown()
+ 	{
+        audio.Play();
+        InvokeRepeating("Accelerate", 0, 3);
+    }
+}
