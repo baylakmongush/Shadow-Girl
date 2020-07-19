0112eca425c7180db80cc32eb5bcf6414d116c36
Ning Petrelli <npetrell@mi-r3.msk.21-school.ru>
2020-07-19 09:13:49 +0300

    edit present time

---
+++ Prototipe/Assets/Scripts/PlayScene/FireFlare.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlare : MonoBehaviour
+{
+    static bool click;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        click = true;
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+		if (click)
+        {
+            animator.SetBool("turn", true);
+            click = false;
+        }
+        else
+        {
+            animator.SetBool("turn", false);
+            click = true;
+        }
+    }
+}
